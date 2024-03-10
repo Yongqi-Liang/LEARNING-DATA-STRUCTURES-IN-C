6d767539af4288d38e9f51f666c7d6177b1e6cc9
Yongqi Liang <my@liangyongqi.top>
2024-03-10 12:21:43 +0800

    new file:   .vscode/tasks.json new file:   time-complexity.c new file:   time-complexity.exe

---
+++ time-complexity.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void printFirstCharacter(char* str){
+    printf("%c",str[0]);
+}
+
+int main(){
+    char str[] = "Liangyongqi";
+    printFirstCharacter(str);
+    return 0;
+}
+
+
+/*
+    时间复杂度  1
+    Output      L
+*/
